87caa2edf8eb01bb0ac88e65b01fe57a7014b581
bonnysid <bonnysidworker@gmail.com>
2026-02-13 19:48:07 +0400

    Improved `formatNumber` to truncate decimals accurately; bumped version to 1.6.4

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -1,5 +1,9 @@
-export const formatNumber = (n: number | string, digits = 2): string =>
-  new Intl.NumberFormat('en-US', {
+export const formatNumber = (n: number | string, digits = 2): string => {
+  const nStr = String(n);
+  const trunkedNum = nStr.slice(0, nStr.indexOf('.') + digits + 1);
+
+  return new Intl.NumberFormat('en-US', {
     minimumFractionDigits: digits,
     maximumFractionDigits: digits,
-  }).format(Number(n));
+  }).format(Number(trunkedNum));
+};
