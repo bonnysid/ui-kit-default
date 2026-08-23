3d340718cd7c35bb4300ce348c2ec4f7b600af76
Nikita Bortsov <bonnysidworker@gmail.com>
2026-08-23 20:13:26 +0400

    Up version and remove `appWindowEventBus` export from WindowEventBus module

--- src/services/window-event-bus/index.ts
+++ src/services/window-event-bus/index.ts
@@ -2,4 +2,4 @@ export { EVENT_NAME_SEPARATOR } from './constants';
 
 export type { WindowEventBusOptions, WindowEventListener, WindowEventName } from './types';
 
-export { appWindowEventBus, WindowEventBus } from './window-event-bus';
+export { WindowEventBus } from './window-event-bus';
